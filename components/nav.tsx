82853f2ae5d7eba50dae2b8e06e9fcf8e02807ba
eirueirufu <731358689@qq.com>
2023-09-10 19:04:01 +0800

    feat: favicon

--- components/nav.tsx
+++ components/nav.tsx
@@ -11,6 +11,8 @@ import {
 } from "@nextui-org/react";
 import { ThemeSwitcher } from "./themeSwitcher";
 import { usePathname, useRouter } from "next/navigation";
+import { GithubLogo, Video } from "@phosphor-icons/react";
+import Image from "next/image";
 
 export default function Nav() {
   const router = useRouter();
@@ -24,50 +26,24 @@ export default function Nav() {
           router.push("/");
         }}
       >
-        <AcmeLogo />
-        <p className='font-bold text-inherit'>ACME</p>
+        <Video size={32} weight='bold' />
       </NavbarBrand>
-      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
-        <NavbarItem>
-          <Link color='foreground' href='#'>
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href='#' aria-current='page'>
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color='foreground' href='#'>
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+      <NavbarContent
+        className='hidden sm:flex gap-4'
+        justify='center'
+      ></NavbarContent>
       <NavbarContent justify='end'>
-        <NavbarItem className='flex'>
+        <NavbarItem className='flex gap-3 items-center justify-center'>
           <ThemeSwitcher />
-        </NavbarItem>
-        <NavbarItem className='hidden lg:flex'>
-          <Link href='#'>Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color='primary' href='#' variant='flat'>
-            Sign Up
-          </Button>
+          <a href='https://github.com/eirueirufu/ani'>
+            <GithubLogo
+              size={32}
+              weight='bold'
+              className='rounded-full cursor-pointer'
+            />
+          </a>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
   );
 }
-
-const AcmeLogo = () => (
-  <svg fill='none' height='36' viewBox='0 0 32 32' width='36'>
-    <path
-      clipRule='evenodd'
-      d='M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z'
-      fill='currentColor'
-      fillRule='evenodd'
-    />
-  </svg>
-);
