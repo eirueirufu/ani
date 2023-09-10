4d2b36f42b49df23a98012572f83db182c63012e
eirueirufu <731358689@qq.com>
2023-09-10 11:36:53 +0800

    staff characters page

---
+++ components/staff/staff.tsx
@@ -0,0 +1,182 @@
+"use client";
+
+import { graphql } from "@/lib/aniList";
+import { useQuery } from "@apollo/client";
+import { Image } from "@nextui-org/react";
+
+export const GetStaff = graphql(`
+  query GetStaff(
+    $id: Int
+    $sort: [MediaSort]
+    $characterPage: Int
+    $staffPage: Int
+    $onList: Boolean
+    $type: MediaType
+    $withCharacterRoles: Boolean = false
+    $withStaffRoles: Boolean = false
+  ) {
+    Staff(id: $id) {
+      id
+      name {
+        first
+        middle
+        last
+        full
+        native
+        userPreferred
+        alternative
+      }
+      image {
+        large
+      }
+      description(asHtml: true)
+      favourites
+      isFavourite
+      isFavouriteBlocked
+      age
+      gender
+      yearsActive
+      homeTown
+      bloodType
+      primaryOccupations
+      dateOfBirth {
+        year
+        month
+        day
+      }
+      dateOfDeath {
+        year
+        month
+        day
+      }
+      language: languageV2
+      characterMedia(page: $characterPage, sort: $sort, onList: $onList)
+        @include(if: $withCharacterRoles) {
+        pageInfo {
+          total
+          perPage
+          currentPage
+          lastPage
+          hasNextPage
+        }
+        edges {
+          characterRole
+          characterName
+          node {
+            id
+            type
+            bannerImage
+            isAdult
+            title {
+              userPreferred
+            }
+            coverImage {
+              large
+            }
+            startDate {
+              year
+            }
+            mediaListEntry {
+              id
+              status
+            }
+          }
+          characters {
+            id
+            name {
+              userPreferred
+            }
+            image {
+              large
+            }
+          }
+        }
+      }
+      staffMedia(page: $staffPage, type: $type, sort: $sort, onList: $onList)
+        @include(if: $withStaffRoles) {
+        pageInfo {
+          total
+          perPage
+          currentPage
+          lastPage
+          hasNextPage
+        }
+        edges {
+          staffRole
+          node {
+            id
+            type
+            isAdult
+            title {
+              userPreferred
+            }
+            coverImage {
+              large
+            }
+            mediaListEntry {
+              id
+              status
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+export default function Staff(props: { id: number }) {
+  const { loading, error, data } = useQuery(GetStaff, {
+    variables: {
+      id: props.id,
+    },
+  });
+  if (loading) {
+    return;
+  }
+  if (error) {
+    throw error;
+  }
+
+  return (
+    <div className='flex flex-col mx-auto p-5 md:flex-row gap-5 w-full max-w-5xl self-center'>
+      <div className='shrink-0 self-center md:self-auto'>
+        <Image
+          alt={data?.Staff?.name?.full ?? ""}
+          src={data?.Staff?.image?.large ?? ""}
+          width={196}
+          height={256}
+        />
+      </div>
+      <div className='flex flex-col gap-5 flex-1'>
+        <div>
+          <h1 className='text-2xl font-bold'>{data?.Staff?.name?.full}</h1>
+          <h2 className='text-sm'>{`${data?.Staff?.name?.native},${data?.Staff?.name?.last},${data?.Staff?.name?.userPreferred}`}</h2>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs'>
+            <span className='font-bold'>Birth: </span>
+            <span>{`${data?.Staff?.dateOfBirth?.month} ${data?.Staff?.dateOfBirth?.day},${data?.Staff?.dateOfBirth?.year}`}</span>
+          </p>
+          <p className='text-xs'>
+            <span className='font-bold'>Gender: </span>
+            <span>{`${data?.Staff?.gender}`}</span>
+          </p>
+          <p className='text-xs'>
+            <span className='font-bold'>Years active: </span>
+            <span>{`${data?.Staff?.yearsActive}`}</span>
+          </p>
+          <p className='text-xs'>
+            <span className='font-bold'>Hometown: </span>
+            <span>{`${data?.Staff?.homeTown}`}</span>
+          </p>
+        </div>
+        <div
+          className='text-xs'
+          dangerouslySetInnerHTML={{
+            __html: data?.Staff?.description ?? "",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+}
---
+++ components/staff/staffMedia.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useQuery } from "@apollo/client";
+import { GetStaff } from "./staff";
+import Loading from "../loading";
+import { MediaSort } from "@/lib/aniList/graphql";
+import { useState } from "react";
+import { Pagination, Image, Card, CardBody } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+export function StaffMedia(props: { id: number }) {
+  const router = useRouter();
+
+  const [page, setPage] = useState(1);
+  const { loading, error, data } = useQuery(GetStaff, {
+    variables: {
+      id: props.id,
+      characterPage: page,
+      sort: MediaSort.StartDateDesc,
+      onList: null,
+      withCharacterRoles: true,
+    },
+  });
+  if (loading) {
+    return <Loading className='w-full h-16' />;
+  }
+  if (error) {
+    throw error;
+  }
+
+  return (
+    <div>
+      <div className='grid grid-cols-3 md:grid-cols-6 gap-3 p-3 max-w-6xl'>
+        {data?.Staff?.characterMedia?.edges?.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              isHoverable
+              isPressable
+              onClick={() => {
+                router.push(
+                  `/media/${item?.node?.id}?type=${item?.node?.type}&isAdult=${item?.node?.isAdult}`
+                );
+              }}
+            >
+              <CardBody className='p-0'>
+                <div className='flex flex-row gap-1 items-center justify-center relative'>
+                  {item?.characters?.map((item, index) => {
+                    return (
+                      <Image
+                        key={index}
+                        alt={item?.name?.userPreferred ?? ""}
+                        src={item?.image?.large ?? ""}
+                        width={196}
+                        height={256}
+                        className='object-cover'
+                      />
+                    );
+                  })}
+                  <div className='absolute left-0 bottom-0'>
+                    <Image
+                      key={index}
+                      alt={item?.node?.title?.userPreferred ?? ""}
+                      src={item?.node?.coverImage?.large ?? ""}
+                      width={96}
+                      height={128}
+                      className='border'
+                    />
+                  </div>
+                </div>
+
+                <div className='overflow-hidden p-2'>
+                  <p>
+                    {item?.characters?.map((character, index) => {
+                      return (
+                        <span key={index} className='text-sm font-bold'>
+                          {character?.name?.userPreferred}
+                          {index < (item.characters?.length ?? 0) - 1 && ", "}
+                        </span>
+                      );
+                    })}
+                    <span className='text-xs ml-1'>
+                      {item?.characterRole === "MAIN" && item?.characterRole}
+                    </span>
+                  </p>
+                  <p className='text-xs'>{item?.node?.title?.userPreferred}</p>
+                </div>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </div>
+      <Pagination
+        isCompact
+        showControls
+        total={data?.Staff?.characterMedia?.pageInfo?.lastPage ?? 1}
+        initialPage={1}
+        page={page}
+        onChange={setPage}
+        className='flex justify-center m-3'
+      />
+    </div>
+  );
+}
